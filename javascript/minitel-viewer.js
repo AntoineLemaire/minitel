7e5f2f2a2c366bd84a83bf724e24676485c4439b
AntoineLemaire <antoine@bouffe.io>
2020-03-30 23:28:07 +0200

    Interactive

--- javascript/minitel-viewer.js
+++ javascript/minitel-viewer.js
@@ -1,9 +1,13 @@
 "use strict"
+let emulator = null;
+
 window.addEventListener("load", function() {
-    const emulator = Minitel.startEmulators()[0]
+    emulator = Minitel.startEmulators()[0];
 
     let stream = ""
-    const cstream = "DAeA.AghBwDYAqACASAATAIRglqSwHkBNAggBQEkBRAZUjLiWQA1MGcWwAZbJgczABZXigEA5EcgEFJAsrLABFBQCUFNBfAUBVBRgBGsALqwA4qbXxtFeEIwBGAHSmAhgCcAJgFNEHgOReAHYAZi6BAC4AzmBiGABMjjQA9gCu4QCWQYiRAMZJADYu6W5eYAQYAMyOggCYGEEufKVkGAAszu7evn5h6QC2LikAHkoYAKyOEOldg0OIAA5uSTkAFl7RKhgedV7zQR5hUUIAagBsjoiCSYGIeX3z4aVixwDsF4LriCWRXm4Abi4MtdogRjgAOd6fPrrSKNdZgMjHACcFxoPm29TygUCXhyjzcYAAYhg4CZ4LBUJFkF4ALAAKAZAEgqbS6QBoNmMtn4AA6ZmO6Xy.R8qVuSS68ySkXCLnyAC5EI5kAR6YylV4gA";
+    // let cstream = queryParameters("cstream");
+    let cstream = "DAeA.AghBwDYAqACASAATAIRglqSwHkBNAggBQEkBRAZUjLiWQA1MGcWwAZbJgczABZXigEA5EcgEFJAsrLABFBQCUFNBfAUBVBRgBGsALqwA4qbXxtFeEIwBGAHSmAhgCcAJgFNEHgOReAHYAZi6BAC4AzmBiGABMjjQA9gCu4QCWQYiRAMZJADYu6W5eYAQYAMyOggCYGEEufKVkGAAszu7evn5h6QC2LikAHkoYAKyOEOldg0OIAA5uSTkAFl7RKhgedV7zQR5hUUIAagBsjoiCSYGIeX3z4aVixwDsF4LriCWRXm4Abi4MtdogRjgAOd6fPrrSKNdZgMjHACcFxoPm29TygUCXhyjzcYAAYtgTPBYKhIlIAFBUyl8ADQjPwAB0zMd0vl8j5Urckl15klIuEXPkAFyIRzIACeAFgqWBTAB1OAACQA.gAIfAUIA---";
+
 
     if(cstream) {
         stream = LZString.decompressFromBase64(
@@ -16,6 +20,63 @@ window.addEventListener("load", function() {
     }
 
     for(let i = 0; i < stream.length; i++) {
+        console.log(stream.charCodeAt(i));
         emulator.send([stream.charCodeAt(i)])
     }
-})
+
+});
+
+function sendToEmulator(char) {
+    emulator.send([char])
+}
+
+let keyPressed = [];
+
+function endOfKeyPressed() {
+    if (keyPressed.length < 57) {
+
+        sendToEmulator(14);
+        sendToEmulator(27);
+        sendToEmulator(72);
+        sendToEmulator(95);
+        sendToEmulator(15);
+        sendToEmulator(27);
+        sendToEmulator(73);
+        sendToEmulator(8);
+    }
+}
+
+document.onkeydown = function(evt) {
+    evt = evt || window.event;
+    var charCode = evt.keyCode || evt.which;
+    // console.log(charCode);
+    if (65 <= charCode && charCode <= 90) {
+        if (keyPressed.length < 58) {
+            keyPressed.push(String.fromCharCode(charCode));
+            sendToEmulator(parseInt(charCode));
+            endOfKeyPressed()
+        }
+    }
+    if (charCode === 13) {
+        // Enter
+        alert("Happy April Fools Day");
+    }
+    if (charCode === 32) {
+        // Spance
+        keyPressed.push(String.fromCharCode(32));
+        sendToEmulator(32); // Space
+        endOfKeyPressed();
+    }
+    if (charCode === 8) {
+        // Backspace
+        if (keyPressed.length > 0) {
+
+            sendToEmulator(46); // Dot
+            sendToEmulator(8); // Move left
+            sendToEmulator(8); // Move left
+            endOfKeyPressed();
+            keyPressed.pop();
+        }
+    }
+
+};
